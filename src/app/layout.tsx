c3d5e76549f4a9e997c6f16aada0e24b56bd1cf0
epatjas <elina.patjas@gmail.com>
2024-12-12 13:24:58 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/reset.css'
 import '@/styles/variables.css'
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -9,9 +10,12 @@ const inter = Inter({
   display: 'swap',
 })
 
-export const metadata = {
-  title: 'Lexie - Helpompi tapa harjoitella',
-  description: 'Lexie auttaa sinua harjoittelemaan tehokkasti ja saavuttamaan tavoitteesi.',
+export const metadata: Metadata = {
+  title: 'Lexie - Parempi tapa oppia',
+  description: 'Lexie auttaa sinua harjoittelemaan kokeisiin ja tekemään läksyjä.',
+  icons: {
+    icon: '/images/favicon.png',
+  },
 }
 
 export default function RootLayout({
